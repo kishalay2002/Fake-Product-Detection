5391c73aeeb18e0dd462b742358ba342630d93ae
kishalay2002 <89611611+kishalay2002@users.noreply.github.com>
2022-11-05 16:15:32 +0530

    Update test.sol

--- test.sol
+++ test.sol
@@ -19,7 +19,45 @@ contract veracity is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
-    
+
+    //Enter rating -1 if user does not want to rate it
+    function transferFromNew(address _from,address _to, uint256 _tokenId,int rate) public
+    {
+        uint last_block_time=block.timestamp;
+        require(rate>=-1 && rate<=5);
+        require(_from==msg.sender);
+        require(lastSold[_from]==0 || lastSold[_from]+100<last_block_time);
+        transferFrom(_from,_to,_tokenId);
+        lastSold[_to]=last_block_time;
+        //require(b);
+        if(rate!=-1)
+        {
+            int temp_rating= rating[_from];
+            rating[msg.sender]=(temp_rating+rate)/(count[msg.sender]+1);
+            count[msg.sender]++;
+        }
+    }
+
+    function time() public view returns (uint)
+    {
+        return block.timestamp;
+    }
+
+    function soldTime(address seller) public view returns(uint)
+    {
+        return lastSold[seller];
+    }
+
+    function getRating(address seller) public view returns(int)
+    {
+        return rating[seller];
+    }
+
+    function getNumberOfRatings(address seller) public view returns(int)
+    {
+        return count[seller];
+    }
+
     // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)internal override(ERC721, ERC721Enumerable)
@@ -41,5 +79,4 @@ contract veracity is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable
     {
         return super.supportsInterface(interfaceId);
     }
-
 }
