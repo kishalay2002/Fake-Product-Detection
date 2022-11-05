b402cfdbe3af05bf99f689eb9e7c7caf5e91d356
kishalay2002 <89611611+kishalay2002@users.noreply.github.com>
2022-11-05 12:58:59 +0530

    Update test.sol

--- test.sol
+++ test.sol
@@ -19,5 +19,27 @@ contract veracity is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
+    
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)internal override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) 
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)public view override(ERC721, ERC721URIStorage) returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)public view override(ERC721, ERC721Enumerable) returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
 }
